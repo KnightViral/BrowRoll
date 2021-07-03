90e1be14d3bc12a3c3dc56b2ce22b98c00f573d8
Sokolov Ilya Sergeevich <i.sokolov@haulmont.com>
2021-07-04 01:02:48 +0400

    1.2.2 Правки стилей для кнопок

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Да, это же АУКЦыОН! Теперь со звуком!!! POG 1.2.1");
+        primaryStage.setTitle("Да, это же АУКЦыОН! Теперь со звуком!!! POG 1.2.2");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("resource/pics/browFat.jpg")));
         primaryStage.show();
